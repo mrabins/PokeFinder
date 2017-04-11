5675053fbce0306bf53d34e4022192a4b9ef48a9
Mark Rabins <markrabins@gmail.com>
2017-04-11 16:28:21 -0700

    CollectionView NameLabel

--- PopoverViewController.swift
+++ PopoverViewController.swift
@@ -18,18 +18,15 @@ class PopoverViewController: UIViewController {
     
     var searchActive = false
     
-    var filteredPokemon: [String] = []
+    var filteredPokemon = [String]()
     
     //    var geoFire: GeoFire!
-    var thePokemon = [pokemon]
+    var thePokemon = [String]()
     
     var cellAttributes = CustomCollectionViewCell()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
         
         navigationController?.navigationBar.isHidden = true
         
@@ -70,37 +67,28 @@ class PopoverViewController: UIViewController {
 extension PopoverViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
         // What to do when selected
     }
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if searchActive {
-            print("Filtered poks \(filteredPokemon.count)")
-
+        
+        if searchActive == true {
             return filteredPokemon.count
+        } else {
+            return pokemon.count
         }
-        print("ThePokemon \(thePokemon.count)")
-        return thePokemon.count
     }
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         cellAttributes = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CustomCollectionViewCell
-        if searchActive == true {
-            let filteredFlatPokemon = filteredPokemon.flatMap { $0 }
-            for filteredItems in filteredFlatPokemon {
-                cellAttributes.pokemonNameLabel.text = filteredItems
-            }
-        } else {
-            let flatPokemon = thePokemon.flatMap { $0 }
-            for items in flatPokemon {
-                cellAttributes.pokemonNameLabel.text = items
-                print("ITEMS \(items)")
-            }
-        }
+        cellAttributes.pokemonNameLabel.text = pokemon[indexPath.row].uppercased()
+
         return cellAttributes
     }
+    
 }
 
 extension PopoverViewController: UISearchBarDelegate, UISearchResultsUpdating {
@@ -125,13 +113,15 @@ extension PopoverViewController: UISearchBarDelegate, UISearchResultsUpdating {
     func filterContentForSearchText(_ searchText: String, scope: String = "Pokemon") {
         searchActive = true
         
-        let filteredFlatPokemon = thePokemon.flatMap { $0 }
-
-        filteredPokemon = filteredFlatPokemon.filter ({(PokeAnnotation) -> Bool in
-            let matchedPokemon = (scope == searchText)
-            return matchedPokemon
-        })
-        collectionView.reloadData()
+        
+//        
+//        //        let filteredFlatPokemon = thePokemon.flatMap { $0 }
+//        //
+//                filteredPokemon = pokemon.filter ({(pokemon) -> Bool in
+//                    let matchedPokemon = (scope == searchText)
+//                    return matchedPokemon
+//                })
+//                collectionView.reloadData()
     }
     
     
